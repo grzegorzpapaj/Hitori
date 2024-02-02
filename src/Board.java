77de9be6dad04c79d20ba20607fa96b18c175241
grzegorzpapaj <137798629+grzegorzpapaj@users.noreply.github.com>
2024-02-02 22:45:11 +0100

    Initial commit. Added the whole project.

---
+++ src/Board.java
@@ -0,0 +1,64 @@
+/**
+ * Klasa reprezentująca planszę gry, składająca się z kwadratów z liczbami.
+ * Klasa zawiera metody pozwalające na inicjalizację planszy oraz umieszczenie na niej losowych liczb.
+ */
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.concurrent.ThreadLocalRandom;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Board extends JPanel {
+
+    int size;
+    Cell[][] cells;
+    int[][] values;
+    BoardReader boardReader;
+    public Board(int size) throws IOException {
+        this.size = size;
+        this.setLayout(new GridLayout(size, size));
+        boardReader = new BoardReader(size);
+        this.values = boardReader.czytaj5x5(size);
+        this.cells = new Cell[size][size];
+
+        for(int i = 0; i<size; i++) {
+            for(int j = 0; j<size; j++){
+                Cell cell = new Cell(i, j, values[j][i]);
+                this.cells[i][j] = cell;
+                this.add(cell);
+            }
+        }
+
+        this.setPreferredSize(new Dimension(500,500));
+        this.setBackground(Color.LIGHT_GRAY);
+
+
+        int marginSize = 10;
+        this.setBorder(BorderFactory.createEmptyBorder(marginSize, marginSize, marginSize, marginSize));
+    }
+
+    public Board getBoard(){
+        return this;
+    }
+
+    public void populateAnotherBoard() throws IOException {
+        this.values = boardReader.czytaj5x5(size);
+
+        for(int i = 0; i<size; i++) {
+            for(int j = 0; j<size; j++){
+                cells[i][j].changeValue(values[j][i]);
+            }
+        }
+    }
+
+    public int getSizee(){
+        return size;
+    }
+
+
+    public Cell[][] getCells() {
+        return cells;
+    }
+}
---
+++ src/BoardReader.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+public class BoardReader {
+
+    int size;
+    final int DLUGOSC_PLIKU = 15;
+    BoardReader(int size){
+        this.size = size;
+    }
+
+    int[][] czytaj5x5(int difficulty) throws IOException {
+
+        Random random = new Random();
+        String filename = "resources/" + difficulty + "x" +difficulty + ".txt";
+        int randomIndex = random.nextInt((int) DLUGOSC_PLIKU);
+        String randomLine = Files.readAllLines(Paths.get(filename)).get(randomIndex);
+        String[] splitLine = randomLine.split("");
+        int[] intLine = new int[size*size];
+        for(int i=0;i<splitLine.length;i++){
+            intLine[i] = Integer.parseInt(splitLine[i]);
+        }
+
+        int[][] intLineArray = new int[size][size];
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++){
+                intLineArray[i][j] = intLine[j*size+i];
+            }
+        }
+
+       return intLineArray;
+    }
+
+}
+
